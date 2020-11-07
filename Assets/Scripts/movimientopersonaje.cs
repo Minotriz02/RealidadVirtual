e97c274b34e6b32a62e6ae55a6857c91b02f05b4
juandavidpernett <llamasw5@gmail.com>
2020-11-06 20:34:05 -0500

    Yo Juan David, como programador de mecánicas de juego, programaré el movimiento del usuario dentro del juego.(Se agregó un terreno a la escena para realizar pruebas)

---
+++ Assets/Scripts/movimientopersonaje.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientopersonaje : MonoBehaviour
+{
+    public Camera camarafps;
+
+    public float velocidadh;
+    public float velocidadv;
+
+    float h;
+    float v;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movimiento de la cámara
+        h = velocidadh * Input.GetAxis("Mouse X");
+        v = velocidadv * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, h, 0);
+        camarafps.transform.Rotate(-v, 0, 0);
+
+
+        //Movimiento del personaje
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, 0.02f);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(0, 0, -0.02f);
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.Translate(-0.02f, 0, 0);
+                }
+                else
+                {
+                    if (Input.GetKey(KeyCode.D))
+                    {
+                        transform.Translate(0.02f, 0, 0);
+                    }
+                }
+            }
+
+        }
+    }
+}
