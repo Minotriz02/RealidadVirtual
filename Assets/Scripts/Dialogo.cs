fc59ba9427c62beeb4708c2c8b383acfb1b14aaa
juandavidpernett <llamasw5@gmail.com>
2020-11-08 21:26:07 -0500

    Yo Juan David, como programador de mecánicas de juego, programaré los dialogos del juego.

---
+++ Assets/Scripts/Dialogo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogo : MonoBehaviour
+{
+    //public AudioClip dialogo;
+    public AudioSource fuente;
+    public GameObject personaje;
+    float distancia;
+    public Camera camarapersonaje;
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        fuente = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        distancia = Vector3.Distance(this.transform.position, personaje.transform.position);
+
+        if (distancia <= 2.75f && Input.GetKey(KeyCode.E))
+        {
+            fuente.Play();
+        }
+    }
+}
